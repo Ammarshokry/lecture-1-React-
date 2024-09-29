f97abcbf9b399f1932dfc12a9ff3cf9d6c2d0573
Ammarshokry <ammaramer1445@gmail.com>
2024-09-29 12:32:09 +0300

    lecture 1 React

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component, useRef, useState } from 'react'
+
+export default function App() {
+  const input = useRef()
+  const [list, setlist] = useState([])
+  const delitem = (index)=>{
+    const deletetask = list.filter((_, i) => i !== index)
+    setlist(deletetask) 
+  }
+  return (
+    <div>
+      <input type="text" ref={input} />
+      <button onClick={() => {
+        if(input.current.value === ""){
+          alert("task input is empty")
+        }else{
+          const tasks = [...list]
+          tasks.push(input.current.value)
+          setlist(tasks)
+        }
+      }}>Addtask</button>
+      <ul>
+        {
+          list.map((el, index) => {
+            return <li key={index}>{el}  <button onClick={()=> delitem(index)}>X</button></li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
